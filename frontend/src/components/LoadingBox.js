b81a3b6b83acd956596acd76c56c155a035df0ab
kunal <chhatwanikunal13@gmail.com>
2021-09-02 19:23:35 +0530

    final commit

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+export default function LoadingBox() {
+  return (
+    <div className="loading">
+      <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
+      <span>Loading...</span>
+    </div>
+  );
+}
